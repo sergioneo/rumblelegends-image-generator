cec906eb1927f4e2bcf07c0a95d6f5fe43570931
Neo <sergio.yanez@alumnos.usm.cl>
2018-03-11 03:18:47 -0700

    Support for mock marketplace

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,5 +1,20 @@
 class ImageController < ApplicationController
   def view
-  	render partial: "view", content_type: "image/svg+xml"
+  	colors = ["deepskyblue",
+  		"forestgreen",
+  		"powderblue",
+  		"sandybrown",
+  		"moccasin",
+  		"lightseagreen",
+  		"lemonchiffon",
+  		"darksalmon",
+  		"darkorchid",
+  		"greenyellow",
+  		"blanchedalmond",
+  		"darkgoldenrod",
+  		"steelblue"]
+
+  	color_code = colors[Integer(params[:id]) % 13]
+  	render partial: "view", content_type: "image/svg+xml", locals: {color_code: color_code}
   end
 end
