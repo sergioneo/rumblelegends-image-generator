523744f4977c23d91e90780a8ec9d6066e3c1e01
Sergio Yanez <sergioy@amazon.com>
2018-08-15 09:55:26 -0700

    Dino working fine

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,6 +1,7 @@
 class ImageController < ApplicationController
 
   def dinosaur
+
     require "dna_reader.rb"
 
     svg_params = DnaReader::dna_read_dino(params[:id])
@@ -14,14 +15,31 @@ class ImageController < ApplicationController
     svg_params[:lightPrimary] = colorCombination.lightened_primary
     svg_params[:darkSecondary] = colorCombination.darkened_secondary
 
-  	render partial: "dinosaur", content_type: "image/svg+xml", locals: svg_params
+    if svg_params.values.include?("0")
+      render partial: "dice", content_type: "image/svg+xml"
+    else
+      render partial: "dinosaur", content_type: "image/svg+xml", locals: svg_params
+    end
   end
 
   def unicorn
     require "dna_reader.rb"
 
-    decoded_locals = DnaReader::dna_read_uni(params[:id])
+    svg_params = DnaReader::dna_read_dino(params[:id])
 
-    render partial: "unicorn", content_type: "image/svg+xml", locals: decoded_locals
+    puts svg_params[:colorCombination]
+    colorCombination = ColorCombination.where(external_id: svg_params[:colorCombination]).first
+
+    svg_params[:secondaryColor] = colorCombination.secondary_color
+    svg_params[:primaryColor] = colorCombination.primary_color
+    svg_params[:darkPrimary] = colorCombination.darkened_primary
+    svg_params[:lightPrimary] = colorCombination.lightened_primary
+    svg_params[:darkSecondary] = colorCombination.darkened_secondary
+
+    if svg_params.values.include?("0")
+      render partial: "dice", content_type: "image/svg+xml"
+    else
+      render partial: "unicorn", content_type: "image/svg+xml", locals: svg_params
+    end
   end
 end
